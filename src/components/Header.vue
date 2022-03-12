bcab5d975a325c8b8300e867778bdde8147c8c1e
no <53334539+120percentistaken@users.noreply.github.com>
2022-03-12 17:20:14 +0800

    First commit of task-tracker using vue.js

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="#39a275"/>
+        <Button text="Update Task" color="#194a8d"/>
+        <Button text="Delete Task" color="#df1c44"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button';
+export default{
+    name: 'Header',
+    props: {
+        title:String,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
